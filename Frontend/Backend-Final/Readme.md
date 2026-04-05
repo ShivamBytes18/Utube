2a595f8366bba3b7e11ddccf56c62fd8bb2fbcf3
ShivamBytes18 <su9555181178@gmail.com>
2026-04-06 03:16:45 +0530

    Important Step

--- Frontend/Backend-Final/Readme.md
+++
@@ -1,1 +0,0 @@
-# A youtube + Thread website by Shivam 
--- Frontend/Backend-Final/src/constants.js
+++
@@ -1,6 +0,0 @@
-export const DB_NAME = "videotube"
-
-export const ACCESS_TOKEN_EXPIRY = "1d"
-export const REFRESH_TOKEN_EXPIRY = "10d"
-
-export const MAX_FILE_SIZE = 50 * 1024 * 1024 // 50MB
